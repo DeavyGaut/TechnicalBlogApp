6187c3bc64530621a7a5ae305139b4f33cf51041
deavyanshg <deavyansh@gmail.com>
2020-06-01 00:19:57 +0530

    Boilerplate code removal and addition of repository layer class following single responsibility principle.

--- src/main/java/technicalblog/service/PostService.java
+++ src/main/java/technicalblog/service/PostService.java
@@ -1,22 +1,19 @@
 package technicalblog.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import technicalblog.model.Post;
+import technicalblog.repository.PostRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
-import javax.persistence.TypedQuery;
-import java.sql.*;
-import java.util.ArrayList;
+import java.sql.Connection;
 import java.util.List;
 
 @SuppressWarnings("ALL")
 @Service
 public class PostService {
 
-    @PersistenceUnit(unitName = "techblog")
-    private EntityManagerFactory emf;
+    @Autowired
+    private PostRepository repository;
 
     public PostService() {
         System.out.println("*** PostService ***");
@@ -24,88 +21,14 @@ public class PostService {
 
     public List<Post> getAllPosts() {
 
-        ArrayList<Post> posts = new ArrayList<>();
-
-//        Post post1 = new Post();
-//        post1.setTitle("Post 1");
-//        post1.setBody("Post Body 1");
-//        post1.setDate(new Date());
-//
-//        Post post2 = new Post();
-//        post2.setTitle("Post 2");
-//        post2.setBody("Post Body 2");
-//        post2.setDate(new Date());
-//
-//        Post post3 = new Post();
-//        post3.setTitle("Post 3");
-//        post3.setBody("Post Body 3");
-//        post3.setDate(new Date());
-//
-//        posts.add(post1);
-//        posts.add(post2);
-//        posts.add(post3);
-
-        EntityManager em = emf.createEntityManager();
-
-        TypedQuery<Post> query = em.createQuery("SELECT p from Post p", Post.class);
-        List<Post> resultList = query.getResultList();
-
-//        //loading the driver
-//        Connection connection = null;
-//        try {
-//            Class.forName("org.postgresql.Driver");
-//
-//            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog", "postgres", "245310");
-//            Statement statement = connection.createStatement();
-//            ResultSet rs = statement.executeQuery("SELECT * FROM posts");
-//            while (rs.next()) {
-//                Post post = new Post();
-//                post.setTitle(rs.getString("title"));
-//                post.setBody(rs.getString("body"));
-//                posts.add(post);
-//            }
-//        } catch (ClassNotFoundException | SQLException e) {
-//            e.printStackTrace();
-//        }
-//        finally{
-//            try {
-//                connection.close();
-//            } catch (SQLException throwables) {
-//                throwables.printStackTrace();
-//            }
-//        }
-
-        return resultList;
+        return repository.getAllPosts();
     }
 
     Connection connection = null;
-    public ArrayList<Post> getOnePost() {
-        ArrayList<Post> posts = new ArrayList<>();
 
-        try {
-            Class.forName("org.postgresql.Driver");
+    public Post getOnePost() {
 
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog", "postgres", "245310");
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM posts WHERE id = 4");
-            while (rs.next()) {
-                Post post = new Post();
-                post.setTitle(rs.getString("title"));
-                post.setBody(rs.getString("body"));
-                posts.add(post);
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-        finally{
-            try {
-                connection.close();
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-        }
-
-        return posts;
+        return repository.getLatestPost();
 
     }
 
